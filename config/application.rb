b7c0e75789b59e6375ec6a3da64f3b97b58a9856
fgolfc <cf.ts.fujimoto@gmail.com>
2023-03-12 20:57:19 +0900

    i18n add date style change

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module CdpWebManyoTask
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.generators do |g|
       g.assets false
       g.helper false
@@ -21,7 +25,7 @@ module CdpWebManyoTask
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    # config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
