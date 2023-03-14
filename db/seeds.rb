41cd33c07f11a9926a48d51d1b247f5f2722223a
fgolfc <cf.ts.fujimoto@gmail.com>
2023-03-14 13:09:17 +0900

    seeds fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'date'
+require 'factory_bot_rails'
 
 date = Date.new(2025, 2, 18)
 
