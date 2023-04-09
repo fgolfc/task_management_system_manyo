7fc5e083681dc3063418c2d91f3aaba702f80750
fgolfc <cf.ts.fujimoto@gmail.com>
2023-04-09 16:04:22 +0900

    remove_column admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_03_085804) do
+ActiveRecord::Schema.define(version: 2023_04_09_070243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2023_04_03_085804) do
     t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
-    t.boolean "admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "tasks_count", default: 0, null: false
